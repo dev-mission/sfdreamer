01d02e79690934e1989f6c6820bfaa50f9739b5d
Francis Li <mail@francisli.com>
2021-06-21 09:45:08 -0700

    Add header nav interactivity, move css

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,5 +1,6 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useHistory, Link } from 'react-router-dom';
+import classNames from 'classnames';
 
 import './Header.scss';
 import Api from './Api';
@@ -8,6 +9,7 @@ import { useAuthContext } from './AuthContext';
 function Header() {
   const history = useHistory();
   const { user, setUser } = useAuthContext();
+  const [active, setActive] = useState(false);
 
   useEffect(
     function () {
@@ -37,27 +39,27 @@ function Header() {
             <img src="/images/Logo.png" alt="SF Dreamer" />
           </a>
         </li>
-        <li className="_item collapsed">
+        <li className={classNames('_item collapsed', { active })}>
           <a href="">Dream Act Info</a>
         </li>
-        <li className="_item">
+        <li className={classNames('_item', { active })}>
           <a href="/questions/1">Eligibility Survey</a>
         </li>
-        <li className="_item collapsed">
+        <li className={classNames('_item collapsed', { active })}>
           <a href="">Mock Application</a>
         </li>
-        <li className="_item">
+        <li className={classNames('_item', { active })}>
           <a href="/forms">AB-540 Forms</a>
         </li>
-        <li className="_item">
+        <li className={classNames('_item', { active })}>
           <a href="/resources">Resources</a>
         </li>
-        <li className="_item collapsed">
+        <li className={classNames('_item collapsed', { active })}>
           <a href="/about">About</a>
         </li>
         {user && (
           <>
-            <li className="_item collapsed">
+            <li className={classNames('_item collapsed', { active })}>
               <a href="/logout" onClick={onLogout}>
                 Log out
               </a>
@@ -65,11 +67,11 @@ function Header() {
           </>
         )}
         {!user && (
-          <li className="_item collapsed">
+          <li className={classNames('_item collapsed', { active })}>
             <Link to="/login">Log in</Link>
           </li>
         )}
-        <li className="_toggle">
+        <li className="_toggle" onClick={() => setActive(!active)}>
           <span className="_bars"></span>
         </li>
       </ul>
