480ba3e7334c3c9ae6e8aeba649316c285f3af46
Jesus Arambula <61527697+JesusEArambula@users.noreply.github.com>
2021-08-13 15:14:02 -0700

    Added <p> tags to About.js section paragraphs and changed some of the styling in About.scss

--- client/src/About.js
+++ client/src/About.js
@@ -5,23 +5,19 @@ function About() {
     <main className="about">
       <h1>About SF Dreamer</h1>
       <div className="container">
-        Have you ever felt like you don't belong?
-        <br />
-        <br />
-        You're not alone!
-        <br />
-        <br />
-        There are more than 3 million other Dreamers like you that have lived in the U.S. since their childhood, but still fear that there's
-        no help for them. Back in 2019, a group of interns from Dev/Mission strongly believed that could make an impact in people's lives by
-        gathering information, resources, and support that could help immigrant students like you pursue a higher education or even a
-        possible employment option regardless of your immigration status.
-        <br />
-        <br />
-        This website was created from scratch by other dreamers like you and with the help of other volunteers that work at different tech
-        companies. As you may notice, this is a website that needs more work to be done, but we strongly believe that we can make an impact
-        in people's lives with the information provided.
-        <br />
-        <br />
+        <p>Have you ever felt like you don't belong?</p>
+        <p>You're not alone!</p>
+        <p>
+          There are more than 3 million other Dreamers like you that have lived in the U.S. since their childhood, but still fear that
+          there's no help for them. Back in 2019, a group of interns from Dev/Mission strongly believed that could make an impact in
+          people's lives by gathering information, resources, and support that could help immigrant students like you pursue a higher
+          education or even a possible employment option regardless of your immigration status.
+        </p>
+        <p>
+          This website was created from scratch by other dreamers like you and with the help of other volunteers that work at different tech
+          companies. As you may notice, this is a website that needs more work to be done, but we strongly believe that we can make an
+          impact in people's lives with the information provided.
+        </p>
       </div>
     </main>
   );
