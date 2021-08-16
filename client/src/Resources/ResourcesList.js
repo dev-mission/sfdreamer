a219f6c1939d3ddf37ab42389a4469ac0772b37d
Jesus Arambula <61527697+JesusEArambula@users.noreply.github.com>
2021-08-16 15:26:40 -0700

    Added card stylig to Resources page

--- client/src/Resources/ResourcesList.js
+++ client/src/Resources/ResourcesList.js
@@ -32,7 +32,38 @@ function ResourcesList() {
             </Link>
           </div>
         )}
-        {resources.map((resource) => (
+        <div className="row">
+          {resources.map((resource) => (
+            <Link to={`/resources/${resource.slug}`} className="resource col-md-4">
+              <div className="resource__card">
+                {resource.logo && (
+                  <>
+                    <img className="img-fluid" srcSet={`${resource.logoUrl} 2x`} src={resource.logoUrl} alt={resource.name} />
+                    <br />
+                  </>
+                )}
+                <h3>{resource.name}</h3>
+                <p>{resource.orgtype}</p>
+                <p>{resource.contactperson}</p>
+                <p>{resource.address}</p>
+                <p className="contact-info">{resource.phone}</p>
+                <p className="contact-info">{resource.email}</p>
+                <p className="contact-info">{resource.website}</p>
+                {user && (
+                  <div className="mt-1">
+                    <Link className="btn btn-sm btn-primary me-3" to={`/resources/${resource.id}/edit`}>
+                      Edit
+                    </Link>
+                    <button onClick={() => onDelete(resource)} className="btn btn-sm btn-danger">
+                      Delete
+                    </button>
+                  </div>
+                )}
+              </div>
+            </Link>
+          ))}
+        </div>
+        {/* {resources.map((resource) => (
           <div key={resource.id} className="row">
             <div className="col-sm mb-3">
               {resource.logo && (
@@ -66,7 +97,7 @@ function ResourcesList() {
               )}
             </div>
           </div>
-        ))}
+        ))} */}
       </div>
     </main>
   );
