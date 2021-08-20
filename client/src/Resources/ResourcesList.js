6ecf0779c1612ecb5327d394fbc8798d2014afef
Jesus Arambula <61527697+JesusEArambula@users.noreply.github.com>
2021-08-19 19:37:22 -0700

    Added card styling to Resources page (#61)
    * Added card stylig to Resources page
    
    * Forgot to remove the commented code
    
    * I could not get the footer fixed at the bottom, but I made the footer stick with the webpage, until I find another solution
    
    * Fixed the footer problem - removed the row height
    
    * Forgot to remove the comments... oopsies

--- client/src/Resources/ResourcesList.js
+++ client/src/Resources/ResourcesList.js
@@ -32,41 +32,37 @@ function ResourcesList() {
             </Link>
           </div>
         )}
-        {resources.map((resource) => (
-          <div key={resource.id} className="row">
-            <div className="col-sm mb-3">
-              {resource.logo && (
-                <>
-                  <img className="img-fluid" srcSet={`${resource.logoUrl} 2x`} src={resource.logoUrl} alt={resource.name} />
-                  <br />
-                </>
-              )}
-              {resource.name}
-              <br />
-              {resource.orgtype}
-              <br />
-              {resource.contactperson}
-              <br />
-              {resource.address}
-              <br />
-              {resource.phone}
-              <br />
-              {resource.email}
-              <br />
-              {resource.website}
-              {user && (
-                <div className="mt-1">
-                  <Link className="btn btn-sm btn-primary me-3" to={`/resources/${resource.id}/edit`}>
-                    Edit
-                  </Link>
-                  <button onClick={() => onDelete(resource)} className="btn btn-sm btn-danger">
-                    Delete
-                  </button>
-                </div>
-              )}
-            </div>
-          </div>
-        ))}
+        <div className="row">
+          {resources.map((resource) => (
+            <Link to={`/resources/${resource.slug}`} className="resource col-md-4">
+              <div className="resource__card">
+                {resource.logo && (
+                  <>
+                    <img className="img-fluid" srcSet={`${resource.logoUrl} 2x`} src={resource.logoUrl} alt={resource.name} />
+                    <br />
+                  </>
+                )}
+                <h3>{resource.name}</h3>
+                <p>{resource.orgtype}</p>
+                <p>{resource.contactperson}</p>
+                <p>{resource.address}</p>
+                <p className="contact-info">{resource.phone}</p>
+                <p className="contact-info">{resource.email}</p>
+                <p className="contact-info">{resource.website}</p>
+                {user && (
+                  <div className="mt-1">
+                    <Link className="btn btn-sm btn-primary me-3" to={`/resources/${resource.id}/edit`}>
+                      Edit
+                    </Link>
+                    <button onClick={() => onDelete(resource)} className="btn btn-sm btn-danger">
+                      Delete
+                    </button>
+                  </div>
+                )}
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </main>
   );
